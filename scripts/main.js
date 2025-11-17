443a327d412f0ed520a11654a84c75622d2a0492
Ishaku Lawan <lawanishaku14@gmail.com>
2025-11-17 15:11:55 +0100

    save course data in localStorage

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,77 @@
-const firstSemesterCourses = [
-  {
-    coursecode: "CSC 300",
-    grade: "B",
-    unit: 2
-  },
-
-  {
-    coursecode: "CSC 300",
-    grade: "B",
-    unit: 2
-  },
-
-  {
-    coursecode: "CSC 300",
-    grade: "B",
-    unit: 2
-  }
-];
-
-const secondSemesterCourses = [];
+const firstSemesterCourses = JSON.parse(localStorage.getItem('firstSemesterCourses')) || [];
+const secondSemesterCourses = JSON.parse(localStorage.getItem('secondSemesterCourses')) || [];
 
 let currentLevel = null;
 let previousCgpa = null;
-let semester = 'first'
+let semester = 'first';
+
+renderCourses();
+
+function getCourses() {
+  const courseCodeElement = document.getElementById('js-course-code');
+  const unitElement = document.getElementById('js-unit');
+  const gradeElement = document.getElementById('js-grade')
+
+  const coursecode = courseCodeElement.value;
+  const unit = Number(unitElement.value);
+  const grade = gradeElement.value;
+
+  if(!coursecode || !unit || !grade) {
+    alert('Please fill all the fields');
+    return;
+  }
+
+  if(semester === 'first') {
+    firstSemesterCourses.push({
+      coursecode,
+      unit,
+      grade
+    });
+    
+  } else if (semester === 'second') {
+    secondSemesterCourses.push({
+      coursecode,
+      unit,
+      grade
+    });
+  }
+
+  localStorage.setItem('firstSemesterCourses', JSON.stringify(firstSemesterCourses));
+  localStorage.setItem('secondSemesterCourses', JSON.stringify(secondSemesterCourses));
+
+  courseCodeElement.value = '';
+  unitElement.value = '';
+  gradeElement.value = '';
+}
+
+//this rendercourses function displays the courses on the page dependinng on the semester that is active
+function renderCourses() {
+  let coursesHtml = '';
+
+  const courses = semester === 'first'? firstSemesterCourses: secondSemesterCourses;
+
+  courses.forEach((course, i) => {
+    coursesHtml += `
+      <div class="course">
+        <div>${i + 1}.</div>
+        <div>${course.coursecode}</div>
+        <div>${course.unit}</div>
+        <div>${course.grade}</div>
+        <img src="icons/delete-icon.svg" alt="delete icon" class="delete-icon js-delete-icon">
+      </div>
+    `;
+  });
+  document.querySelector('.js-course-display-container').innerHTML = coursesHtml;
+
+  document.querySelectorAll('.js-delete-icon').forEach((button, i) => {
+    button.addEventListener('click', ()=> {
+      courses.splice(i, 1);
+      localStorage.setItem(courses === firstSemesterCourses? 'firstSemesterCourses': 'secondSemesterCourses', JSON.stringify(courses));
+      console.log(firstSemesterCourses)
+      renderCourses();
+    });
+  });
+}
 
 //first and second semester toggle 
 const firstSemToggleElm = document.querySelector('.first-semester-courses');
@@ -60,45 +109,14 @@ semesterElement.addEventListener('change', (event)=> {
   renderCourses();
 });
 
-function getCourses() {
-  const courseCodeElement = document.getElementById('js-course-code');
-  const unitElement = document.getElementById('js-unit');
-  const gradeElement = document.getElementById('js-grade')
-
-  const coursecode = courseCodeElement.value;
-  const unit = Number(unitElement.value);
-  const grade = gradeElement.value;
-
-  if(!coursecode || !unit || !grade) {
-    alert('Please fill all the fields');
-    return;
-  }
-
-  if(semester === 'first') {
-    firstSemesterCourses.push({
-      coursecode,
-      unit,
-      grade
-    });
-    
-  } else if (semester === 'second') {
-    secondSemesterCourses.push({
-      coursecode,
-      unit,
-      grade
-    });
-  }
-
-  console.log(firstSemesterCourses)
-
-  courseCodeElement.value = '';
-  unitElement.value = '';
-  gradeElement.value = '';
-}
-
 document.getElementById('js-add-button').addEventListener('click', ()=> {
   getCourses();
   renderCourses();
+});
+
+
+document.getElementById('js-calculate').addEventListener('click', ()=> {
+  renderGpa();
 });
 
 function calculateGpa() {
@@ -144,37 +162,6 @@ function calculateCgpa() {
   }
 
   return cgpa;
-}
-
-document.getElementById('js-calculate').addEventListener('click', ()=> {
-  renderGpa();
-});
-
-//this rendercourses function displays the courses on the page dependinng on the semester that is active
-function renderCourses() {
-  let coursesHtml = '';
-
-  const courses = semester === 'first'? firstSemesterCourses: secondSemesterCourses;
-
-  courses.forEach((course, i) => {
-    coursesHtml += `
-      <div class="course">
-        <div>${i + 1}.</div>
-        <div>${course.coursecode}</div>
-        <div>${course.unit}</div>
-        <div>${course.grade}</div>
-        <img src="icons/delete-icon.svg" alt="delete icon" class="delete-icon js-delete-icon">
-      </div>
-    `;
-  });
-  document.querySelector('.js-course-display-container').innerHTML = coursesHtml;
-
-  document.querySelectorAll('.js-delete-icon').forEach((button, i) => {
-    button.addEventListener('click', ()=> {
-      courses.splice(i, 1);
-      renderCourses();
-    });
-  });
 }
 
 //this block of code just displays the gpa for a semester on the page
